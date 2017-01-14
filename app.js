394bb4d93ef7aee5a96262fe9ae318d11b708ab3
TomGarrison <Tomgarrison2011@gmail.com>
2017-01-14 12:57:10 -0700

    update

--- app.js
+++ app.js
@@ -1,39 +1,53 @@
-var play = document.getElementById('play')
-play.addEventListener("click",playGame)
-function playGame() {
-    alert('Press "OK" To Play A Programming Language Hangman Game');
-    var words = ["javascript", "jquery", "nodejs", "coding", "php", "java", "python", "ruby", "objectivec"];
-    //random word
-    var word = words[Math.floor(Math.random() * words.length)];
-    //answer array
-    var answerArray = [];
-    for (var i = 0; i < word.length; i++) {
-        answerArray[i] = "_";
-    }
-    var remainingLetters = word.length;
-    // game loop
-    while (remainingLetters > 0) {
-        //progress
-        alert(answerArray.join(" "));
-        // Get a guess from the player
-        var guess = prompt("Guess one letter, or click Cancel to stop playing.");
-        if (guess === null) {
-            // Exit the game loop
-            break;
-        } else if (guess.length !== 1) {
-            alert("Please enter a single letter!");
-        } else {
-            // update guess
-            for (var j = 0; j < word.length; j++) {
-                if (word[j] === guess) {
-                    answerArray[j] = guess;
-                    remainingLetters--;
-                }
-            }
-        }
-        // End game loop
-    }
-    // The answer
-    alert(answerArray.join(" "));
-    alert("The answer was " + word);
-}
+// <script>
+// var play = document.getElementById('play')
+// play.addEventListener("click", playGame)
+// function playGame() {
+//     function pickWord() {
+//       var words = ["javascript", "jquery", "nodejs", "coding", "php", "java", "python", "ruby", "objectivec"];
+//       return words[Math.floor(Math.random() * words.length)];
+//     };
+//     function setupAnswerArray(word) {
+//       var answerArray = [];
+//       for (i=0; i < word.length; i++) {
+//         answerArray[i] = "_";
+//       }
+//       return answerArray;
+//     };
+//     function showPlayerProgress(answerArray) {
+//       alert(answerArray.join(" "));
+//     };
+//     function getGuess() {
+//       var guess = prompt("Guess a letter, or click Cancel to stop playing");
+//       return guess = guess.toLowerCase();
+//     };
+//     function updateGameState(guess, word, answerArray) {
+//       var numberOfGuesses = 0;
+//       for (var j = 0; j < word.length; j++) {
+//         if (word[j] === guess) {
+//           answerArray[j] = guess;
+//           numberOfGuesses++;
+//         }
+//       }
+//       return numberOfGuesses;
+//     };
+//     function showAnswerAndCongratulatePlayer(answerArray) {
+//         alert("Decent! The answer was " + (answerArray.join("")));
+//     };
+//     var word = pickWord();
+//     var answerArray = setupAnswerArray(word);
+//     var remainingLetters = word.length;
+//     while (remainingLetters > 0) {
+//       showPlayerProgress(answerArray);
+//       var guess = getGuess();
+//       if (guess === null) {
+//         break;
+//       } else if (guess.length !== 1) {
+//         alert("Please enter a single letter.");
+//       } else {
+//             var correctGuesses = updateGameState(guess, word, answerArray);
+//             remainingLetters -= correctGuesses;
+//           }
+//         };
+//         showAnswerAndCongratulatePlayer(answerArray);
+// }
+// </script>
